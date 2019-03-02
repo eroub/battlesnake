511e8864d93d3f2b762764a771aa80a0b8318ec3
Evan Roubekas <35752039+eroub@users.noreply.github.com>
2019-03-02 21:32:14 +0000

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,88 @@
+import json
+import os
+import random
+import bottle
+
+from api import ping_response, start_response, move_response, end_response
+
+@bottle.route('/')
+def index():
+    return '''
+    Battlesnake documentation can be found at
+       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
+    '''
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    """
+    Given a path, return the static file located relative
+    to the static folder.
+    This can be used to return the snake head URL in an API response.
+    """
+    return bottle.static_file(path, root='static/')
+
+@bottle.post('/ping')
+def ping():
+    """
+    A keep-alive endpoint used to prevent cloud application platforms,
+    such as Heroku, from sleeping the application instance.
+    """
+    return ping_response()
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+
+    """
+    TODO: If you intend to have a stateful snake AI,
+            initialize your snake state here using the
+            request's data if necessary.
+    """
+    print(json.dumps(data))
+
+    color = "#E3256B"
+
+    return start_response(color)
+
+
+
+@bottle.post('/move')
+def move():
+    direction = ['up', 'down', 'left', 'right']
+    data = bottle.request.json
+
+    head = data['you']['body'][0]
+
+    #left_wall = {'x': 0, 'y': }
+
+
+    print(json.dumps(data))
+
+    directions = ['up', 'down', 'left', 'right']
+    direction = random.choice(directions)
+
+    return move_response(direction)
+
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    """
+    TODO: If your snake AI was stateful,
+        clean up any stateful objects here.
+    """
+    print(json.dumps(data))
+
+    return end_response()
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+
+if __name__ == '__main__':
+    bottle.run(
+        application,
+        host=os.getenv('IP', 'http://roubekas.com'),
+        port=os.getenv('PORT', '8080'),
+        debug=os.getenv('DEBUG', True)
+    )
